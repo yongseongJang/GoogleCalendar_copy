965c9e8f6dbacf3e7e6e877f8a14bce2a57c331c
yongseongJang <kgucs13@gmail.com>
2022-06-13 19:45:29 +0900

    feat(client): publishing part of Calendar component using emotion (AnnualCalendar)

--- src/client/components/AnnualCalendar/Calendar/component.tsx
+++ src/client/components/AnnualCalendar/Calendar/component.tsx
@@ -8,6 +8,59 @@ interface CalendarProps {
   month: number;
 }
 
+const calendar = css`
+  min-width: 288px;
+  min-height: 252px;
+  flex-basis: 25%;
+
+  > div {
+    width: 256px;
+    padding: 0 16px 16px 16px;
+    position: relative;
+    @media (max-width: 1500px) {
+      width: 208px;
+    }
+  }
+
+  @media (max-width: 1500px) {
+    min-width: 224px;
+  }
+`;
+
+const monthly = css`
+  height: 32px;
+  font-size: 12px;
+  font-weight: 400;
+  margin: 0px 4px;
+
+  > span {
+    color: #70757a;
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing: 0.25px;
+    line-height: 20px;
+    padding-left: 5px;
+  }
+`;
+
+const table = css`
+  display: table;
+  width: 100%;
+`;
+
+const tableRow = css`
+  display: table-row;
+  height: 28px;
+`;
+
+const tableCell = css`
+  font-size: 12px;
+  font-weight: 400;
+  display: table-cell;
+  vertical-align: middle;
+  color: #70757a;
+`;
+
 const days = ["일", "월", "화", "수", "목", "금", "토"];
 
 function Calendar(props: CalendarProps) {
@@ -16,17 +69,24 @@ function Calendar(props: CalendarProps) {
   const lastDayOfMonth = dayjs(year + "-" + month).daysInMonth();
 
   return (
-    <div>
+    <div css={calendar}>
       <div>
-        <span
-          css={css`
-            color: #70757a;
-          `}
-        >{`${month}월`}</span>
+        <div css={monthly}>
+          <span>{`${month}월`}</span>
+        </div>
+        <div css={table}>
+          <div css={tableRow}>
+            {days.map((day) => (
+              <span key={day} css={tableCell}>
+                {day}
+              </span>
+            ))}
+          </div>
+          <div>
+            <div>{}</div>
+          </div>
+        </div>
       </div>
-      {days.map((day) => (
-        <span key={day}>{day}</span>
-      ))}
     </div>
   );
 }
