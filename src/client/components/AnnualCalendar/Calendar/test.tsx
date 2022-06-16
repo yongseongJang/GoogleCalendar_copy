a4d91c744f71e1f0041abe1c3fba5962273d9098
yongseongJang <kgucs13@gmail.com>
2022-06-17 06:09:08 +0900

    test(client): add Calendar test code

--- src/client/components/AnnualCalendar/Calendar/test.tsx
+++ src/client/components/AnnualCalendar/Calendar/test.tsx
@@ -1,17 +1,22 @@
 import * as React from "react";
 import { render } from "../../../utils/test-utils";
 import { Calendar } from "../../AnnualCalendar/Calendar";
+import dayjs from "dayjs";
 
 const renderCalendar = () => {
+  const day = dayjs("2022-06-17");
   const result = render(<Calendar year={2022} month={6} />);
+
+  const dateOfToday = () => result.getByText(day.date());
 
   return {
     result,
+    dateOfToday,
   };
 };
 
 describe("<Calendar />", () => {
-  it("renders Calendar component correctly", () => {
+  it("renders day correctly", () => {
     const { result } = renderCalendar();
 
     const day = ["일", "월", "화", "수", "목", "금", "토"];
@@ -19,5 +24,11 @@ describe("<Calendar />", () => {
     day.forEach((d) => {
       expect(result.getByText(d)).toBeInTheDocument();
     });
+  });
+
+  it("renders today correctly", () => {
+    const { dateOfToday } = renderCalendar();
+
+    expect(dateOfToday()).toHaveStyle("background-color: #1a73e8");
   });
 });
