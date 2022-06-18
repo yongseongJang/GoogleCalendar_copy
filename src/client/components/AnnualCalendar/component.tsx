fd98bc5ee1eb7b4f475ae2bb85e41f71f72179ca
yongseongJang <kgucs13@gmail.com>
2022-06-18 19:55:45 +0900

    feat(client): apply css AnnualCalendar

--- src/client/components/AnnualCalendar/component.tsx
+++ src/client/components/AnnualCalendar/component.tsx
@@ -1,6 +1,8 @@
+/** @jsxImportSource @emotion/react */
 import * as React from "react";
 import dayjs from "dayjs";
 import { Calendar } from "../AnnualCalendar/Calendar";
+import { css } from "@emotion/react";
 
 function AnnualCalendar() {
   const month = Array(12)
@@ -9,12 +11,27 @@ function AnnualCalendar() {
   const year = dayjs().year();
 
   return (
-    <>
-      {month.map((m) => (
-        <Calendar key={m} year={year} month={m} />
-      ))}
-    </>
+    <div css={annualCalendar}>
+      <div css={flexWrap}>
+        {month.map((m) => (
+          <Calendar key={m} year={year} month={m} />
+        ))}
+      </div>
+    </div>
   );
 }
+
+const annualCalendar = css`
+  flex: 1;
+`;
+
+const flexWrap = css`
+  padding-right = 32px;
+  padding-top: 8px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-content: flex-start;
+`;
 
 export default AnnualCalendar;
